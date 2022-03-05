eea7721c226d9dcad001ce6bf8d6382d4a1441bf
Dominic Chu <dominic.m.chu@gmail.com>
2022-03-05 08:55:35 -0500

    calledByName and calledByValue notes

--- src/main/scala/lectures/part1basics/CBNvsCNV.scala
+++ src/main/scala/lectures/part1basics/CBNvsCNV.scala
@@ -2,7 +2,20 @@ package lectures.part1basics
 
 object CBNvsCNV extends App {
 
+    def calledByValue(x: Long): Unit = {
+        // when called by value, the value of input is passed, not the input object
+        println("by value " + x)
+        println("by value " + x)
+    }
+
+    def calledByName(x: => Long): Unit = {
+        // when called by name, the entire input is passed whole so System.nanoTime() is passed and not the value of System.nanoTime() as time of passing
+        println("by value: " + x)
+        println("by value: " + x)
+    }
 
 
+    calledByValue(System.nanoTime())
+    calledByName(System.nanoTime())
 
 }
